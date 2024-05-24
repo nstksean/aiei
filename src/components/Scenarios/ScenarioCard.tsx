12cc1df49fb2c0bc6111624d779ee7ae89822478
seann <seannlin@ieiworld.com>
2024-05-24 17:47:22 +0800

    [FRONTEND] Add nnCard

--- src/components/Scenarios/ScenarioCard.tsx
+++ src/components/Scenarios/ScenarioCard.tsx
@@ -5,7 +5,100 @@ import { Button } from "../ui/button"
 
 import useNewTask from "../../stores/useNewTask"
 
+const NNCard = ()=>{
+  return(
+  <div className=" block rounded-lg bg-white  text-surface shadow-secondary-2 overflow-auto">
+    <div className="text-base  text-primary font-medium" >
+      <h2 className="text-lg text-zinc-500 font-semibold py-1">{'Inputs ：'}</h2>
+      <p>{'Image, name: '}<br/>
+        <code className="">
+          <span className=" bg-slate-100  text-red-500"> {' image '} </span>
+          </code><br/>
+          {', shape: '} 
+        <code className=" ">
+          <span className=" bg-slate-100  text-red-500">{' 1, '}</span> 
+          <span className=" bg-slate-100  text-red-500">{'3,'}</span> 
+          <span className=" bg-slate-100  text-red-500">{' 720, '}</span> 
+          <span className=" bg-slate-100  text-red-500">{' 1280 '}</span>
+          </code><br/>
+          {' in the format '} 
+        <code className=" ">
+          <span className=" bg-slate-100  text-red-500">{' B, '}</span> 
+          <span className=" bg-slate-100  text-red-500">{' C, '}</span> 
+          <span className=" bg-slate-100  text-red-500">{' H, '}</span> 
+          <span className=" bg-slate-100  text-red-500">{' W '}</span>
+        </code>
+        {' , where: '}
+      </p>
+      <ul className=" ">
+      <li><p><code className=" "><span className=" bg-slate-100  text-red-500">{' B '}</span></code>{' - batch size'}</p></li>
+      <li><p><code className=" "><span className=" bg-slate-100  text-red-500">{' C '}</span></code>{' - number of channels'}</p></li>
+      <li><p><code className=" "><span className=" bg-slate-100  text-red-500">{' H '}</span></code>{' - image height'}</p></li>
+      <li><p><code className=" "><span className=" bg-slate-100  text-red-500">{' W '}</span></code>{' - image width'}</p></li>
+      </ul>
+      <p>{'Expected color order is '}
+        <code className=" ">
+          <span className="bg-slate-100  text-red-500 ">{' BGR '}</span>
+        </code>.
+      </p>
+    </div>
+    <div className="text-base  text-primary font-medium ">
+      <h2 className="text-lg text-zinc-500 font-semibold py-1">{'Outputs ：'}</h2>
+      <ol className="pb-2">
+      <li><p >{'The'} 
+        <code className=" "><span className=" bg-slate-100  text-red-500 ">{' boxes '}</span></code>
+        {' is a blob with the shape '}
+        <code className="">
+          <span className="bg-slate-100  text-red-500  ">{' 100, '}</span> 
+          <span className=" bg-slate-100  text-red-500 ">{' 5 '}</span>
+        </code>
+        {' in the format '}
+        <code className=" ">
+          <span className=" bg-slate-100  text-red-500 ">{' N, '}</span> 
+          <span className="bg-slate-100  text-red-500  ">{' 5 '}</span></code>
+        {', where '}
+        <code className=" ">
+          <span className=" bg-slate-100  text-red-500 ">{' N '}</span>
+        </code> 
+        {'is the number of detected bounding boxes.'}<br/>
+        {'For each detection, the description has the format:'}
+          <code className="  "><span className=" bg-slate-100  text-red-500">{'[ x_min '}</span></code>{', '} 
+          <code className="  "><span className=" bg-slate-100  text-red-500">{' y_min '}</span></code>{', '}
+          <code className="  "><span className=" bg-slate-100  text-red-500">{' x_max '}</span></code>{', '}
+          <code className="  "><span className=" bg-slate-100  text-red-500">{' y_max '}</span></code>{', '}
+          <code className="  "><span className=" bg-slate-100  text-red-500">{' conf ]'}</span></code>
+          {', where:'}</p>
+      <ul className=" ">
+      <li><p>(<code className="  "><span className=" bg-slate-100  text-red-500">{' x_min '}</span></code>{', '}
+              <code className="  "><span className=" bg-slate-100  text-red-500">{' y_min '}</span></code>)
+              {' - coordinates of the top left bounding box corner'}
+      </p></li>
+      <li><p>(<code className="  "><span className=" bg-slate-100  text-red-500">{' x_max '}</span></code>{', '}
+      <code className="  "><span className=" bg-slate-100  text-red-500">{' y_max '}</span></code>) 
+      {' - coordinates of the bottom right bounding box corner.'}
+      </p></li>
+      <li><p><code className="  "><span className=" bg-slate-100  text-red-500">{' conf '}</span></code>{' - confidence for the  dicted className'}</p></li>
+      </ul>
+      </li>
+      <li><p>{'The'} 
+        <code className="  "><span className=" bg-slate-100 text-red-500">{' labels '}</span></code>
+        {' is a blob with the shape '}
+        <code className="  "><span className=" bg-slate-100  text-red-500">{' 100 '}</span></code> 
+        {' in the format '}
+        <code className="  "><span className=" bg-slate-100  text-red-500">{' N '}</span></code>
+        {', where '}
+        <code className="  "><span className="bg-slate-100  text-red-500 ">{' N '}</span></code> 
+        {'is the number of detected bounding boxes. In case of person detection, it is equal to '}
+        <code className="  "><span className=" bg-slate-100  text-red-500">{' 1 '}</span></code> 
+        {'for each detected box with person in it and '}
+        <code className="  "><span className=" bg-slate-100  text-red-500">{' 0 '}</span></code> 
+        {'for the background.'}
+      </p></li>
+      </ol>
+    </div>
+  </div>
 
+)}
 
 export default function ScenarioCard({
   chosenRow,
@@ -22,6 +115,7 @@ export default function ScenarioCard({
 
   useEffect(() => {
     setChosenScenario(chosenRow)
+    return()=>console.log(chosenScenario)
   }, [chosenRow])
 
   let scenarioId = chosenScenario?.id ? chosenScenario.id : " "
@@ -91,25 +185,24 @@ export default function ScenarioCard({
       <div className="border border-stroke overflow-y-auto basis-3/5 h-full bg-slate-50 shadow-default rounded-md max-h-[740px] min-h-[360px] flex flex-col justify-between px-5 pt-2">
         {/* Scenario intro */}
         <div className="flex flex-col text-clip">
-          <p className="text-xs text-blue-600 font-medium">Scenario ID：{scenarioId}</p>
-          <p className="text-2xl text-blue-600 font-medium">{scenarioTitle}</p>
-          <p className="text-base text-blue-600 font-medium">
+          <p className="text-base  text-sky-700 font-medium my-1">Scenario ID：{scenarioId}</p>
+          <p className="text-2xl  text-primary font-medium my-1">{scenarioTitle}</p>
+          <p className="text-base  text-primary font-medium my-1">
             introduction：
           </p>
-          <p className="text-base text-blue-600 font-medium">
+          <p className="text-base  text-zinc-700 font-medium mt-1" >
             {scenarioIntro}
           </p>
         </div>
         {/* nn and event */}
-        <div className=" rounded-md overflow-y-auto max-h-[580px] min-h-96 flex gap-2 ">
-          <div className="basis-1/2 shadow-default bg-white p-2 flex flex-col overflow-y-auto">
-            <p className="text-base text-blue-600 font-medium bg-white mb-1 shadow-default">Pipeline Information</p>
-
-            <p className="text-base text-zinc-700 font-bold bg-white">NN：</p>
-            <p className="text-base text-zinc-700 bg-white">{scenarioNN}</p>
+        <div className=" rounded-md overflow-y-auto max-h-[580px] min-h-96 flex gap-2 my-2">
+          <div className="basis-2/3 shadow-default bg-white p-2 flex flex-col overflow-y-auto">
+            <p className="text-base  text-sky-700 font-medium bg-white mb-1 shadow-default">Pipeline Information</p>
+            <p className="text-base text-zinc-700 font-bold bg-white py-1">NN：{scenarioNN}</p>
+            {scenarioNN? <NNCard/> : ''}
           </div>
-          <div className="basis-1/2  shadow-default bg-white p-2 flex flex-col overflow-y-auto">
-            <p className="text-base text-blue-600 font-medium bg-white mb-1 shadow-default">Event:</p>
+          <div className="basis-1/3  shadow-default bg-white p-2 flex flex-col overflow-y-auto">
+            <p className="text-base  text-sky-700 font-medium bg-white mb-1 shadow-default">Event:</p>
             
               {/* Event container */}
               <div className="flex flex-col gap-2 bg-white shadow-default">
@@ -134,7 +227,7 @@ export default function ScenarioCard({
             </div>
         </div>
         {/* next button */}
-        <div className="flex flex-row-reverse min-h-10 pb-2 px-2">
+        <div className="flex flex-row-reverse min-h-10 pb-2 px-2 my-2">
           <NextButton disabled={allowNext} handleNextClick={onNextClick}/>
           {/* test Zustand */}
           <Button 
